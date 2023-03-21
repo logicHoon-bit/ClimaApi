cf1dca45e05edccaf8b06562fda08939561d5bbd
이치훈 <dlclgns152615@gmail.com>
2023-03-22 02:23:41 +0900

    final.

--- ClimaAPI/Controller/WeatherViewController.swift
+++ ClimaAPI/Controller/WeatherViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import CoreLocation
 //import SnapKit
-//test
 
 class WeatherViewController: UIViewController {
 
@@ -24,12 +23,22 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self //위치를 요청하기전에 delegate 설정해야됨
         locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation() //일회성 주소 요청
+        //locationManager.startUpdatingLocation()//연속성주소요청
+       
         
         searchTextField.delegate = self
         weatherManager.delegate = self
+       
     }
-
+    
+    
+    @IBAction func showLocationWeather(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
 }
 
 //MARK: - UITextFieldDelegate
@@ -80,6 +89,27 @@ extension WeatherViewController: WeatherManagerDelegate {
     }
     
     func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print("\(lat), \(lon)")
+            
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
     
